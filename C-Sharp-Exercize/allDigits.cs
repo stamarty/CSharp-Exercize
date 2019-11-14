79cae0b24cfac56078bcd43a86170b144f93941a
stamarty <7009764+stamarty@users.noreply.github.com>
2019-11-13 20:47:30 -0800

    update cci_ReverseString with comments.

--- C-Sharp-Exercize/allDigits.cs
+++ C-Sharp-Exercize/allDigits.cs
@@ -6,47 +6,50 @@ using System.Threading.Tasks;
 
 namespace C_Sharp_Exercize
 {
-    
-    /*
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Program test = new Program();
-            Console.WriteLine(test.allDigits("here is a string with no digits"));
-            Console.WriteLine(test.allDigits("1111111"));
 
-        }
-    */
 
-        // 
-        // Student: Steven Martinez
-        // 
-        // Genesis: Question was asked during a practice whiteboarding session from Microsoft Mentors 
-        // Summary: Checks a string to see if the value of the string is all digits. Returns false if there is a letter in there.
-        // returns bool
-        public bool allDigits(string s)
-        {
-            //using try/catch to return false if error during parse.
-            try
-            {
-                foreach (char c in s)
-                {
-                    int.Parse(c.ToString());
-                }
-            }
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        AllDigits test = new AllDigits();
+    //        Console.WriteLine(test.allDigits("here is a string with no digits"));
+    //        Console.WriteLine(test.allDigits("1111111"));
 
-            catch (Exception)
-            {
-                return false;
-            }
+    //    }
 
-            return true;
-        }
 
-        // Output:
-        // False
-        // True
+    //    // 
+    //    // Student: Steven Martinez
+    //    // 
+    //    // Genesis: Question was asked during a practice whiteboarding session from Microsoft Mentors 
+    //    // Summary: Checks a string to see if the value of the string is all digits. Returns false if there is a letter in there.
+    //    // returns bool
 
-    }
+    //    class AllDigits
+    //{
+    //    public bool allDigits(string s)
+    //    {
+    //        //using try/catch to return false if error during parse.
+    //        try
+    //        {
+    //            foreach (char c in s)
+    //            {
+    //                int.Parse(c.ToString());
+    //            }
+    //        }
+
+    //        catch (Exception)
+    //        {
+    //            return false;
+    //        }
+
+    //        return true;
+    //    }
+    //}
+    //    // Output:
+    //    // False
+    //    // True
+
+    //}
 }
--- C-Sharp-Exercize/cci_ReverseString.cs
+++ C-Sharp-Exercize/cci_ReverseString.cs
@@ -6,36 +6,59 @@ using System.Threading.Tasks;
 
 namespace C_Sharp_Exercize
 {
-    class cci_ReverseString
-    {
-        //reverses string entered as parameter and prints to console. 
-        public void PrintReverseString(string str)
-        {
-            char[] arr = str.ToCharArray();
-            for (int i = 0, j = arr.Length - 1 ; i < j; i++, j--)
-            {
-                char temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        cci_ReverseString test = new cci_ReverseString();
 
-            string reversedString = new string(arr);
-            Console.WriteLine(reversedString);
-        }
+    //        // prints to the console as part of the constructor
+    //        test.PrintReverseString("Here's a string to turn around");
 
-        //returns the string entered as a parameter reversed. 
-        public string ReverseString(string str)
-        {
-            char[] arr = str.ToCharArray();
-            for (int i = 0, j = arr.Length - 1; i < j; i++, j--)
-            {
-                char temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+    //        // prints to the console from the return in the second constructor
+    //        Console.WriteLine(test.ReverseString("here's a second string"));
 
-            string reversedString = new string(arr);
-            return reversedString;
-        }
-    }
+
+    //    }
+    //}
+
+    //class cci_ReverseString
+    //{
+    //    // reverses string entered as parameter and prints to console. 
+    //    // I am experimenting with building different constructors and passing
+    //    // values into each to see how building them works.
+    //    public void PrintReverseString(string str)
+    //    {
+    //        // turn string into char array, cycle through the array and replace first char with last etc....
+    //        char[] arr = str.ToCharArray();
+    //        for (int i = 0, j = arr.Length - 1 ; i < j; i++, j--)
+    //        {
+    //            char temp = arr[i];
+    //            arr[i] = arr[j];
+    //            arr[j] = temp;
+    //        }
+
+    //        string reversedString = new string(arr);
+    //        Console.WriteLine(reversedString);
+    //    }
+
+    //    // returns the string entered as a parameter reversed.
+    //    // turn string into char array, cycle through the array and replace first char with last etc....
+    //    public string ReverseString(string str)
+    //    {
+    //        char[] arr = str.ToCharArray();
+    //        for (int i = 0, j = arr.Length - 1; i < j; i++, j--)
+    //        {
+    //            char temp = arr[i];
+    //            arr[i] = arr[j];
+    //            arr[j] = temp;
+    //        }
+
+    //        string reversedString = new string(arr);
+    //        return reversedString;
+    //    }
+    //}
+
+    //// Output - dnuora nrut ot gnirts a s'ereH
+    //// Output - gnirts dnoces a s'ereh
 }
