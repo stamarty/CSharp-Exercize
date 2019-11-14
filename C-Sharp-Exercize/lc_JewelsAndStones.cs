78f70b6cdab5ef8c727e4eab3e32a4a181edd99e
stamarty <7009764+stamarty@users.noreply.github.com>
2019-11-13 21:26:05 -0800

    lc_JewelsAndStones updated

--- C-Sharp-Exercize/lc_JewelsAndStones.cs
+++ C-Sharp-Exercize/lc_JewelsAndStones.cs
@@ -6,13 +6,29 @@ using System.Threading.Tasks;
 
 namespace C_Sharp_Exercize
 {
-    // You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
-    // Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
-    //
-    // The letters in J are guaranteed distinct, and all characters in J and S are letters.Letters are case sensitive, 
-    // so "a" is considered a different type of stone from "A".
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        lc_JewelsAndStones test = new lc_JewelsAndStones();
+
+    //        Console.WriteLine(test.NumJewelsInStones("AaBc", "aaBBccAAddeeff"));
+
+    //    }
+    //}
+
+
     class lc_JewelsAndStones
     {
+        // Student: Steven Martinez
+        //
+        // Genesis: This coding problem is from leetcode.com
+        //
+        // Summary: You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
+        // Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+        //
+        // The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, 
+        // so "a" is considered a different type of stone from "A".
         public int NumJewelsInStones(string J, string S)
         {
             int counter = 0;
@@ -29,4 +45,6 @@ namespace C_Sharp_Exercize
             return counter;
         }
     }
+
+    // Output: 8
 }
