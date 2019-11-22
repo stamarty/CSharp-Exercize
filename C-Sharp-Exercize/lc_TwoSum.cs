71d98a93d5123e098688f3309ba14c1ee5fe1d90
stamarty <7009764+stamarty@users.noreply.github.com>
2019-11-21 20:48:06 -0800

    add lc_TwoSum.cs

---
+++ C-Sharp-Exercize/lc_TwoSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Exercize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class lc_TwoSum
+    {
+        // Student: Steven Martinez
+        //
+        // Genesis: This problem is in Leetcode.com
+        //
+        // Summary: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+        // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        //
+
+
+
+    }   
+}
