cec6073b10f8c2b06598902c665bd4659c8384d4
stamarty <7009764+stamarty@users.noreply.github.com>
2019-11-29 20:29:22 -0800

    Begin NRepeatedElements from Leetcode

---
+++ C-Sharp-Exercize/lc_NRepeatedElements2NArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace C_Sharp_Exercize
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        // commented out for copy/paste and ease of building other classes
+    //        // for this project
+
+    //    }
+    //}
+
+
+    // Student: Steven Martinez
+    //
+    // Genesis: This problem is in Leetcode.com
+    // Link: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+    //
+    // Summary: In an array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+    // Return the element repeated N times.
+
+    public class lc_NRepeatedElements2NArray
+    {
+        public int RepeatedNTimes(int[] A)
+        {
+
+        }
+    }
+}
