8fc3677d42706649bb6af3d8d3af901c675ef610
doomedcow <Gymlet@users.noreply.github.com>
2019-12-01 12:48:31 -0800

    finish TopKFrequent class. works, but i think string override needs to be implemented.

--- C-Sharp-Exercize/TopKFrequent.cs
+++ C-Sharp-Exercize/TopKFrequent.cs
@@ -7,18 +7,17 @@ using System.IO;
 
 namespace C_Sharp_Exercize
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            lc_TopKFrequent test = new lc_TopKFrequent();
-            string[] arr1 = { "i", "love", "leetcode", "i", "love", "coding" };
-            int k = 2;
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        lc_TopKFrequent test = new lc_TopKFrequent();
+    //        string[] arr1 = { "i", "love", "leetcode", "i", "love", "coding" };
+    //        int k = 2;
 
-            Console.WriteLine(test.TopKFrequent(arr1, k));
-
-        }
-    }
+    //        Console.WriteLine(test.TopKFrequent(arr1, k));
+    //    }
+    //}
 
     // Student: Steven Martinez
     //
@@ -32,37 +31,49 @@ namespace C_Sharp_Exercize
 
     public class lc_TopKFrequent
     {
+        // method returns a list of K length with the top k most frequent words
         public IList<string> TopKFrequent(string[] words, int k)
         {
+            // create dictionary from the string array words
             IDictionary<string, int> myDict = new Dictionary<string, int>();
 
+            // cycle through the array
             foreach (var word in words)
             {
+                // if dictionary already contains the word
                 if (myDict.ContainsKey(word))
                 {
+                    // increment the value by 1
                     myDict[word]++;
                 }
 
+                // if the dictionary does not contain the word
                 else
                 {
+                    // add the word intialize the value to 1
                     myDict.Add(word, 1);
                 }
 
-                IList<string> KFrequency = new List<string>();
-
-                foreach (var item in myDict.OrderByDescending(R => R.Value).ThenBy(R => R.Key))
-                {
-                    if (k>0)
-                    {
-                        KFrequency.Add(item.Key);
-                        k--;
-                    }
-                }
-
-                return KFrequency;
             }
 
-            return null;
+            // create a list to hold the key from dictionary
+            IList<string> KFrequency = new List<string>();
+
+            // cycle through the dictionary. order the list by value, then alphabetically by key
+            foreach (var item in myDict.OrderByDescending(R => R.Value).ThenBy(R => R.Key))
+            {
+                // do it as many times as k is
+                if (k > 0)
+                {
+                    // add key to the list
+                    KFrequency.Add(item.Key);
+                    //decrement k
+                    k--;
+                }
+            }
+
+            // return the list. The list should only contain k elements
+            return KFrequency;
         }
     }
 }
